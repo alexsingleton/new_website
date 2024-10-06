9b6d78bd9908a265d0599d8a296da8c9ab1a3e00
Alex Singleton <alexandersingleton@bmail.com>
2024-10-06 21:24:20 +0000

    update for git fix

--- setup.R
+++ setup.R
@@ -5,3 +5,6 @@ new_site(theme = "hugo-apero")
 
 #git config --global user.name 'Alex Singleton'
 #git config --global user.email 'alexandersingleton@bmail.com'
+
+# stops rstudio asking for u & p on push
+#git config remote.origin.url git@github.com:alexsingleton/new_website.git 
