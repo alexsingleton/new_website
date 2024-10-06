6e8d1a7a115a2e12921ae1fadff9f6bd1c41a4c8
Alex Singleton <alexandersingleton@bmail.com>
2024-10-06 21:13:11 +0000

    setup update

--- setup.R
+++ setup.R
@@ -1,2 +1,7 @@
 remotes::install_github("rstudio/blogdown")
 library(blogdown)
+new_site(theme = "hugo-apero")
+
+
+#git config --global user.name 'Alex Singleton'
+#git config --global user.email 'alexandersingleton@bmail.com'
